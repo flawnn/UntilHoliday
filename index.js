3c0a3cea98709f5889e4e2b3ee3014b3862d56a6
Baran <baran.peters@lessing-ffm.net>
2018-03-26 23:38:46 +0200

    chanes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
   });
 })
 
-app.listen(8000, function () {
+app.listen( function () {
   console.log('Example app listening on port 8000!')
 })
 function getTime() {
